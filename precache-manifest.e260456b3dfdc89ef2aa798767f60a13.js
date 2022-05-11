73bf464a97c583440e2b3daa90789b816d61c1de
cagkaizen010 <kaizenrodriguez@gmail.com>
2022-05-11 17:59:06 -0400

    Updates

---
+++ precache-manifest.e260456b3dfdc89ef2aa798767f60a13.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f753ecf3e7e639d58a42351145f2c7d",
+    "url": "/yanapii.art/index.html"
+  },
+  {
+    "revision": "835a2c35bfaac70d8594",
+    "url": "/yanapii.art/static/js/2.bac77f1f.chunk.js"
+  },
+  {
+    "revision": "4c50516b1879334b3823",
+    "url": "/yanapii.art/static/js/main.0376a209.chunk.js"
+  },
+  {
+    "revision": "17d931f1204c1728ca38",
+    "url": "/yanapii.art/static/js/runtime-main.ead18357.js"
+  }
+]);
